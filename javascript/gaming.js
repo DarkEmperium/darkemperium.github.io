ad35555a56a17185c42955d868ae016c0aab7697
DarkEmperium <jundechua2003@gmail.com>
2024-02-01 14:55:57 +0800

    Update Content

--- javascript/gaming.js
+++ javascript/gaming.js
@@ -16,3 +16,20 @@ function reveal() {
 
 window.addEventListener("scroll", reveal);
 
+
+// Get the current date
+const currentDate = new Date();
+
+// Get the current month (0-indexed, so we add 1)
+const currentMonth = currentDate.getMonth() + 1;
+
+// Get the game version element
+const gameVersionElement = document.getElementById('gameVersion');
+
+// Update the game version based on the current month
+if (currentMonth >= 2) { // Update from February onwards
+  const currentVersion = gameVersionElement.innerText.match(/\d+\.\d+/);
+  const newVersion = parseFloat(currentVersion) + 0.1 * (currentMonth - 1);
+  gameVersionElement.innerText = `Genshin Impact Version ${newVersion.toFixed(1)} Live Now !`;
+}
+
