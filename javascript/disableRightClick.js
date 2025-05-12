92d211c21b7fe89c47e92f7d656cbb2b3c7a8552
Dark Emperium <101817945+DarkEmperium@users.noreply.github.com>
2025-05-12 22:25:15 +0800

    Fix some bugs

--- javascript/disableRightClick.js
+++ javascript/disableRightClick.js
@@ -1,11 +1,5 @@
 var show_msg = '';
 
-if (show_msg !== '0') {
-    var options = {view_src: "View Source is disabled!", inspect_elem: "Inspect Element is disabled!", right_click: "Right click is disabled!", copy_cut_paste_content: "Cut/Copy/Paste is disabled!", image_drop: "Image Drag-n-Drop is disabled!" }
-} else {
-    var options = '';
-}
-
 function nocontextmenu(e) { return false; }
 document.oncontextmenu = nocontextmenu;
 document.ondragstart = function() { return false;}
@@ -13,35 +7,28 @@ document.ondragstart = function() { return false;}
 document.onmousedown = function (event) {
     event = (event || window.event);
     if (event.keyCode === 123) {
-        if (show_msg !== '0') {show_toast('inspect_elem');}
         return false;
     }
 }
+
 document.onkeydown = function (event) {
     event = (event || window.event);
-    //alert(event.keyCode);   return false;
     if (event.keyCode === 123 ||
-            event.ctrlKey && event.shiftKey && event.keyCode === 73 ||
-            event.ctrlKey && event.shiftKey && event.keyCode === 75) {
-        if (show_msg !== '0') {show_toast('inspect_elem');}
-        return false;
-    }
-    if (event.ctrlKey && event.keyCode === 85) {
-        if (show_msg !== '0') {show_toast('view_src');}
+        event.ctrlKey && event.shiftKey && (event.keyCode === 73 || event.keyCode === 75) ||
+        event.ctrlKey && event.keyCode === 85) {
         return false;
     }
 }
-function addMultiEventListener(element, eventNames, listener) {
+
+function addMultiEventListener(element, eventNames) {
     var events = eventNames.split(' ');
     for (var i = 0, iLen = events.length; i < iLen; i++) {
         element.addEventListener(events[i], function (e) {
             e.preventDefault();
-            if (show_msg !== '0') {
-                show_toast(listener);
-            }
         });
     }
 }
-addMultiEventListener(document, 'contextmenu', 'right_click');
-addMultiEventListener(document, 'cut copy paste print', 'copy_cut_paste_content');
-addMultiEventListener(document, 'drag drop', 'image_drop');
+
+addMultiEventListener(document, 'contextmenu');
+addMultiEventListener(document, 'cut copy paste print');
+addMultiEventListener(document, 'drag drop');
