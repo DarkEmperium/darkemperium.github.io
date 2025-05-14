edd853e15f32c3d233de721a34c132dcb079e333
Dark Emperium <101817945+DarkEmperium@users.noreply.github.com>
2025-05-14 15:39:01 +0800

    Update some content in about me page

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,3 @@
-/*====== MENU =====*/
 const showMenu = (toggleId,navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
@@ -11,37 +10,30 @@ const showMenu = (toggleId,navId) =>{
 }
 showMenu('nav-toggle','nav-menu')
 
-/*====== RELLAX =====*/
 var rellax = new Rellax('.parallax');
 
-
-/*====== ANIMATE GSAP ======*/
-/*Navbar*/
 gsap.from('.nav__logo', {opacity:0, duration: 3, delay: .5, y: 30, ease:'expo.out'});
 gsap.from('.nav__toggle', {opacity:0, duration: 3, delay: .7, y: 30, ease:'expo.out'});
 gsap.from('.nav__item', {opacity: 0, duration: 3, delay: .7, y: 35, ease:'expo.out', stagger: .2})
-
-/*Text*/
 gsap.from('.home__title', {opacity:0, duration: 3, delay: 1.3, y: 35, ease:'expo.out'});
-gsap.from('.home__subtitle', {opacity:0, duration: 3, delay: 1.1 , y: 35, ease:'expo.out'});
-
-/*Scroll*/
 gsap.from('.home__scroll', {opacity:0, duration: 3, delay: 1.5, y: 25, ease:'expo.out'});
 
-
-/*====== SCROLL REVEAL SECTION ======*/
 const sr = ScrollReveal({
     duration: 2500,
     reset: true
 });
 
-/*Data*/
 sr.reveal('.section',{origin: 'left',distance: '70px'}); 
-/*Skill*/
 sr.reveal('.section_skill',{origin: 'left',distance: '70px'}); 
-/*Favourite Video*/
 sr.reveal('.video-container',{origin: 'bottom',distance: '70px'}); 
-/*Favourite Places*/
 sr.reveal('.favourite-places',{origin: 'left',distance: '70px'}); 
-/*Imgs*/
 sr.reveal('.section__img',{origin: 'left',distance: '90px',delay: 200}); 
+
+document.addEventListener('DOMContentLoaded', function () {
+    var typed = new Typed('#auto-type', {
+        strings: ['Full Stack Web Developer', 'Computer Technician', 'Tech Enthusiast', 'System Administrator'],
+        typeSpeed: 80,
+        loop: true,
+        backSpeed: 80,
+    });
+});
